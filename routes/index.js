41b10ee2620fbbdadd8cb8123decfd2a95f96bd8
Hataipan Weerapin <nutpedd23@hotmail.com>
2019-07-14 10:35:10 +0700

    เพิ่เลเอ้าท์

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+router.get('/create', function(req, res, next) {
+  res.render('create', );
+});
 module.exports = router;
